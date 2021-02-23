24247763ff05a0540dbc3d8b15af416a9cd9e83c
Matt <mattfsewell@gmail.com>
2021-02-23 12:03:02 -0500

    commit tree fe1ca993755e4de7baa01cef1388b582fcdf2ec0

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your code here!
+let newHeader;
+let element = document.createElement('div') 
+document.body.appendChild(element)
+
+let ul = document.createElement('ul')
+
+for (let i = 0; i < 3; i++){
+    let li = document.createElement('li')
+    li.innerHTML = (i + 1).toString()
+    ul.appendChild(li)
+}
+element.appendChild(ul)
+element.style.textAlign = 'center';
+ul.style.textAlign = 'left' 
+element.style.backgroundColor = '#27647B'; 
+
+main.remove();
+newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory");
+document.body.appendChild(newHeader)
+
+// newHeader = document.getElementById("h1#victory");
+
+victory.innerHTML = "YOUR-NAME is the champion"
